5e67d4d5ee8c4779e806a6aad096661d523b116c
XenZi <rilakmateja@gmail.com>
2024-01-16 10:25:54 +0100

    add finished exercises of rustlings

---
+++ exercises/clippy/target/debug/deps/clippy3-6a10437844c427ac.d
@@ -0,0 +1,9 @@
+/Users/matejarilak/rustlings/exercises/clippy/target/debug/deps/clippy3-6a10437844c427ac.rmeta: clippy3.rs Cargo.toml
+
+/Users/matejarilak/rustlings/exercises/clippy/target/debug/deps/clippy3-6a10437844c427ac.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
